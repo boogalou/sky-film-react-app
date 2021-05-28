33889d22db2e90af7a8be2ca2318168abe4a941d
pat_holman <tommypo26@gmail.com>
2021-05-28 18:45:29 +0300

    Added paly button

--- src/components/Main/Showcase-Item/Showcase-Item.jsx
+++ src/components/Main/Showcase-Item/Showcase-Item.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import './Showcase-Item.css'
 import {apiConfig} from '../../../config'
+import PlayButton from "../../common/Play-button/Play-button";
 
 
-const ShowcaseItem = ({ backdrop_path, title, mouseSpyHandler,  }) => {
+const ShowcaseItem = ({backdrop_path, title, mouseSpyHandler,}) => {
   const backdrop = apiConfig.image.w780ImgURL + backdrop_path
   return (
     <>
+
       <li className='showcase-container'>
         <div className="showcase-box">
-          <img  src={backdrop} alt="movie poster"
-               onMouseMove={mouseSpyHandler} />
+
+          <img src={backdrop} alt="movie poster"
+               onMouseMove={mouseSpyHandler}/>
+          <PlayButton className='play-button'/>
         </div>
-          <div className='title-container'><span className='movie-title'>{title}</span></div>
+        <div className='title-container'>
+          <span className='movie-title'>
+            {title}
+          </span></div>
       </li>
+
     </>
   )
 }
